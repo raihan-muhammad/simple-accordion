57b0a3a3af723bd6cf20ee4d54c00ecfc9313113
raihan-muhammad <sukasukangoding@gmail.com>
2024-01-12 20:21:45 +0700

    feat: add toggle handle function

--- src/Accordion.jsx
+++ src/Accordion.jsx
@@ -4,7 +4,7 @@ export default function Accordion({ data }) {
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccordionItem title={el.title} text={el.text} num={i} />
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
       ))}
     </div>
   );
